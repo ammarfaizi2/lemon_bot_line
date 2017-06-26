226124215d1de176c47a7664bfa6d3f9bc3bd830
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-26 16:00:11 +0700

    Fix route

--- src/LINEBot/EchoBot/Route.php
+++ src/LINEBot/EchoBot/Route.php
@@ -44,7 +44,7 @@ class Route
                 return $res->withStatus(400, 'Bad Request');
             }*/
             $body = $req->getBody();
-            $body = '{"events":[{"type":"message","replyToken":"5c32e7193d4e4ebf9f9326a656babeb6","source":{"userId":"U547ba62dc793c6557abbb42ab347f15f","type":"user"},"timestamp":1498463825764,"message":{"type":"text","id":"6296397218198","text":"Halo"}}]}';
+            /*$body = '{"events":[{"type":"message","replyToken":"5c32e7193d4e4ebf9f9326a656babeb6","source":{"userId":"U547ba62dc793c6557abbb42ab347f15f","type":"user"},"timestamp":1498463825764,"message":{"type":"text","id":"6296397218198","text":"q_anime ordinal scale"}}]}';*/
 
             $body = json_decode($body, true);
             $ai = new AI();
@@ -57,178 +57,19 @@ class Route
                         if (is_array($reply)) {
                             $build = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($reply[0], $reply[0]);   
                             $bot->pushMessage($event['source']['userId'], $build);
-                            $build = new \LINE\LINEBot\MessageBuilder\TestMessageBuilder($reply[1]);   
+                            $build = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($reply[1]);   
                             $bot->pushMessage($event['source']['userId'], $build);
                         } else {
-                            $build = new \LINE\LINEBot\MessageBuilder\TestMessageBuilder($reply);   
+                            $build = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($reply);   
                             $bot->pushMessage($event['source']['userId'], $build);
                         }
                     } else {
-                        $build = new \LINE\LINEBot\MessageBuilder\TestMessageBuilder("Mohon maaf saya belum mengerti \"{$event['message']['text']}\"");   
+                        $build = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("Mohon maaf saya belum mengerti \"{$event['message']['text']}\"");   
                         $bot->pushMessage($event['source']['userId'], $build);
                     }
                 }
             }
             #$res->write($body);
-            return $res;
-        });
-
-
-
-
-
-
-
-
-
-
-        $app->get('/callbackz', function (\Slim\Http\Request $req, \Slim\Http\Response $res) {
-            
-            /** 
-             * @var \LINE\LINEBot $bot 
-             */
-            $bot = $this->bot;
-            
-            /** 
-             * @var \Monolog\Logger $logger 
-             */
-            $logger = $this->logger;
-
-            $signature = $req->getHeader(HTTPHeader::LINE_SIGNATURE);
-            /*if (empty($signature)) {
-                return $res->withStatus(400, 'Bad Request');
-            }*/
-            #$body = $req->getBody();
-            file_put_contents("body.txt", $body);
-            /*// Check request with signature and parse request
-            $body = json_decode($body,true);
-            $body = $body['events'];
-            foreach ($body as $event) {
-                if ($event['type']==="message" and $event['message']['type']==="text") {
-                    $ai = new AI();
-                    $st = $ai->prepare($event['message']['text']);
-                    if ($st->execute()) {
-                        $replyText = $st->fetch_reply();
-                    } else {
-                        $replyText = "Mohon maaf saya belum mengerti \"{$getText}\"";
-                    }
-                    file_put_contents("debug_reply.txt", json_encode($replyText, 128));
-                  
-                    if (is_array($replyText)) {
-                        $imageMessageBuilder = (new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($replyText[0], $replyText[1]));
-                        file_put_contents("event_debug_replyzz.txt", json_encode($event, 128));
-                        $ss = $bot->pushMessage(/*$event['source']['userId']*//*"U547ba62dc793c6557abbb42ab347f15f", $imageMessageBuilder);
-                        var_dump($ss);
-                        $logger->info('Reply text: ' . $replyText[1]);
-                        $resp = $bot->replyText($event['replyToken'], $replyText[1]);
-                    } else {
-                        $logger->info('Reply text: ' . $replyText);
-                        $resp = $bot->replyText($event['replyToken'], $replyText);
-                    }
-                    $logger->info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
-                } else {
-                    continue;
-                }
-            }*/
-          /*  
-            try {
-                $events = $bot->parseEventRequest($body, $signature[0]);
-            } catch (InvalidSignatureException $e) {
-                return $res->withStatus(400, 'Invalid signature');
-            } catch (InvalidEventRequestException $e) {
-                return $res->withStatus(400, "Invalid event request");
-            }*/
-
-           /* foreach ($events as $event) {
-                if (!($event instanceof MessageEvent)) {
-                    $logger->info('Non message event has come');
-                    continue;
-                }
-
-                if (!($event instanceof TextMessage)) {
-                    $logger->info('Non text message has come');
-                    continue;
-                }
-
-                $getText = $event->getText();
-                $ai = new AI();
-                $st = $ai->prepare($getText);
-                if ($st->execute()) {
-                    $replyText = $st->fetch_reply();
-                } else {
-                    $replyText = "Mohon maaf saya belum mengerti \"{$getText}\"";
-                }
-                file_put_contents("debug_reply.txt", json_encode($replyText, 128));
-                if (is_array($replyText)) {
-                    $imageMessageBuilder = (new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($replyText[0], $replyText[0]));
-                    file_put_contents("event_debug_replyzz.txt", json_encode($event, 128));
-                    $bot->pushMessage($event['source']['userId'], $imageMessageBuilder);
-                    $logger->info('Reply text: ' . $replyText[1]);
-                    $resp = $bot->replyText($event->getReplyToken(), $replyText[1]);
-                } else {
-                    $logger->info('Reply text: ' . $replyText);
-                    $resp = $bot->replyText($event->getReplyToken(), $replyText);
-                }
-                $logger->info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
-            }*/
-
-            $res->write('OK');
-            return $res;
-        });
-        $app->post('/callbackp', function (\Slim\Http\Request $req, \Slim\Http\Response $res) {
-            
-            /** 
-             * @var \LINE\LINEBot $bot 
-             */
-            $bot = $this->bot;
-            
-            /** 
-             * @var \Monolog\Logger $logger 
-             */
-            $logger = $this->logger;
-
-            $signature = $req->getHeader(HTTPHeader::LINE_SIGNATURE);
-            if (empty($signature)) {
-                return $res->withStatus(400, 'Bad Request');
-            }
-
-            // Check request with signature and parse request
-            try {
-                $events = $bot->parseEventRequest($req->getBody(), $signature[0]);
-            } catch (InvalidSignatureException $e) {
-                return $res->withStatus(400, 'Invalid signature');
-            } catch (InvalidEventRequestException $e) {
-                return $res->withStatus(400, "Invalid event request");
-            }
-
-            foreach ($events as $event) {
-                if (!($event instanceof MessageEvent)) {
-                    $logger->info('Non message event has come');
-                    continue;
-                }
-
-                if (!($event instanceof TextMessage)) {
-                    $logger->info('Non text message has come');
-                    continue;
-                }
-
-                $getText = $event->getText();
-                file_put_contents("test.txt", $getText);
-                $ai = new AI();
-                $st = $ai->prepare($getText);
-                if ($st->execute()) {
-                    $replyText = $st->fetch_reply();
-                    $replyText = is_array($replyText) ? json_encode($replyText) : $replyText;
-                } else {
-                    $replyText = "Mohon maaf saya belum mengerti \"{$getText}\"";
-                }
-                file_put_contents("reply.txt", $replyText);
-                $logger->info('Reply text: ' . $replyText);
-                $resp = $bot->replyText($event->getReplyToken(), $replyText);
-                $logger->info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
-            }
-
-            $res->write('OK');
             return $res;
         });
     }
